99de230fe2ad9e093c1f3e9b24e31cbbc3cbf5fc
Akmaliin <akmalnafis25@gmail.com>
2024-03-09 01:26:00 +0700

    [Task-4] Adding catalog model and adpater

--- app/src/main/java/com/example/a24001084_km6_akm_warungpenyet_ch2/MainActivity.kt
+++ app/src/main/java/com/example/a24001084_km6_akm_warungpenyet_ch2/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.Toast
+import com.example.a24001084_km6_akm_warungpenyet_ch2.adapter.CatalogAdapter
 import com.example.a24001084_km6_akm_warungpenyet_ch2.adapter.CategoryAdapter
 import com.example.a24001084_km6_akm_warungpenyet_ch2.databinding.ActivityMainBinding
+import com.example.a24001084_km6_akm_warungpenyet_ch2.model.Catalog
 import com.example.a24001084_km6_akm_warungpenyet_ch2.model.Category
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val adapterCategory = CategoryAdapter()
+    private val adapterCatalog = CatalogAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -35,5 +38,25 @@ class MainActivity : AppCompatActivity() {
         adapterCategory.submitData(data)
     }
 
+    private fun setListCatalog() {
+        val data = listOf(
+            Catalog(image = R.drawable.img_ayam, name = "Penyetan Ayam", price = 15000.0),
+            Catalog(
+                image = R.drawable.img_ayam_komplit,
+                name = "Penyetan komplit",
+                price = 20000.0
+            ),
+            Catalog(image = R.drawable.img_lele, name = "lele Goreng", price = 15000.0),
+            Catalog(image = R.drawable.img_belut, name = "Penyetan Belut", price = 17000.0),
+            Catalog(image = R.drawable.img_tahu_tempe, name = "Tahu Tempe", price = 12000.0),
+            Catalog(image = R.drawable.img_indomie, name = "Indomie Special", price = 8000.0),
+            Catalog(image = R.drawable.img_esteh, name = "Es teh Manis", price = 3000.0)
+
+        )
+        binding.rvCatalog.apply {
+            adapter = this@MainActivity.adapterCatalog
+        }
+        adapterCatalog.submitData(data)
+    }
 
 }
---
+++ app/src/main/java/com/example/a24001084_km6_akm_warungpenyet_ch2/model/Catalog.kt
@@ -0,0 +1,13 @@
+package com.example.a24001084_km6_akm_warungpenyet_ch2.model
+
+import androidx.annotation.DrawableRes
+import java.util.UUID
+
+data class Catalog(
+    var id: String = UUID.randomUUID().toString(),
+    @DrawableRes
+    var image: Int,
+    var name: String,
+    var price: Double,
+
+    )
